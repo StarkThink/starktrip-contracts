f8746ee6de00f52411cf1c7ee29ef4f759e3defa
Jonatan Chaverri <jonatan.chaverri@kyndryl.com>
2024-06-18 21:09:40 -0600

    update default_auth script

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -2,31 +2,29 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-need_cmd() {
-  if ! check_cmd "$1"; then
-    printf "need '$1' (command not found)"
-    exit 1
-  fi
-}
+if [ $# -ge 1 ]; then
+    export PROFILE=$1
+else
+    export PROFILE="dev"
+fi
 
-check_cmd() {
-  command -v "$1" &>/dev/null
-}
-
-need_cmd jq
-
-export RPC_URL="http://localhost:5050"
-
-export WORLD_ADDRESS=$(cat ./manifests/dev/manifest.json | jq -r '.world.address')
+export WORLD_ADDRESS=$(cat ./manifests/$PROFILE/manifest.json | jq -r '.world.address')
+export GAME_SYSTEM_ADDRESS=$(cat ./manifests/$PROFILE/manifest.json | jq -r '.contracts[] | select(.name == "starktrip::systems::game_system::game_system" ).address')
 
 echo "---------------------------------------------------------------------------"
+echo profile : $PROFILE
+echo "---------------------------------------------------------------------------"
 echo world : $WORLD_ADDRESS
+echo " "
+echo game_system : $GAME_SYSTEM_ADDRESS
 echo "---------------------------------------------------------------------------"
 
 # enable system -> models authorizations
-sozo auth grant --world $WORLD_ADDRESS --wait writer \
-  Position,dojo_starter::systems::actions::actions\
-  Moves,dojo_starter::systems::actions::actions\
-  >/dev/null
-
-echo "Default authorizations have been successfully set."
+sozo -P ${PROFILE} auth grant --world $WORLD_ADDRESS --wait writer \
+  Game,$GAME_SYSTEM_ADDRESS\
+  Board,$GAME_SYSTEM_ADDRESS\
+  Tile,$GAME_SYSTEM_ADDRESS\
+  CharactersInside,$GAME_SYSTEM_ADDRESS\
+  Spaceship,$GAME_SYSTEM_ADDRESS\
+  LeaderBoard,$GAME_SYSTEM_ADDRESS\
+  LeaderBoardPlayers,$GAME_SYSTEM_ADDRESS
